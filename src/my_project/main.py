7e1850857fc67b0ff3b2359c22939910976a6d4d
Konstantin Kuzin <kkuzin01@gmail.com>
2025-08-31 21:17:36 +0300

    update names file

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -1,10 +1,10 @@
 def clear_names(file_name: str) -> list:
     """Функция для очистки имено от лишних символов"""
     new_names_list = list()
-    with open('data/' + file_name, encoding='utf-8') as names_file:
+    with open("data/" + file_name, encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
-            new_name = ''
+            new_name = ""
             for symbol in name_item:
                 if symbol.isalpha():
                     new_name += symbol
@@ -12,8 +12,9 @@ def clear_names(file_name: str) -> list:
                 new_names_list.append(new_name)
     return new_names_list
 
-if __name__ == '__main__':
-    cleared_name = clear_names('names.txt')
+
+if __name__ == "__main__":
+    cleared_name = clear_names("names.txt")
 
     for i in cleared_name:
         print(i)
