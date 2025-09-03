74cc4440500e46f5debc7c8bf767791b553e5d63
Konstantin Kuzin <kkuzin01@gmail.com>
2025-09-03 10:21:25 +0300

    filter for russian names

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -1,7 +1,10 @@
-def clear_names(file_name: str) -> list:
-    """Функция для очистки имено от лишних символов"""
+import re
+
+
+def clear_names(file_name: str) -> list[str]:
+    """Функция для очистки имен от лишних символов"""
     new_names_list = list()
-    with open("data/" + file_name, encoding="utf-8") as names_file:
+    with open("../../data/" + file_name, encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
             new_name = ""
@@ -12,9 +15,21 @@ def clear_names(file_name: str) -> list:
                 new_names_list.append(new_name)
     return new_names_list
 
+def is_cyrillic(name_item: str) -> bool:
+    """Проверка на вхождение кириллицы в строку"""
+    return bool(re.search('[а-яА-Я]', name_item))
+
+
+def filter_russian_name(names_list: list) -> list:
+    """Фильтрация имен написанных на русском"""
+    new_names_list = list()
+    for name_item in names_list:
+        if is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
 
 if __name__ == "__main__":
     cleared_name = clear_names("names.txt")
 
-    for i in cleared_name:
-        print(i)
+    print(filter_russian_name(cleared_name))
