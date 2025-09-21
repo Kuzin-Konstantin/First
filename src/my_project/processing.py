bcb0ecad2d70a48c9bb2abacc03ed974a286502d
Konstantin Kuzin <kkuzin01@gmail.com>
2025-09-21 14:20:06 +0300

    add new func 'filter_by_state'

--- src/my_project/processing.py
+++ src/my_project/processing.py
@@ -1,1 +1,6 @@
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Вернуть только элементы с заданным state."""
+    return [item for item in data if item.get("state") == state]
+
+
 
