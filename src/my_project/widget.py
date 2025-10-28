eb1bc84d3736c649c211fb89fa3566c2b86b13ab
Konstantin Kuzin <kkuzin01@gmail.com>
2025-10-28 11:55:22 +0300

    Создал файл для тестов модуля widget

--- src/my_project/widget.py
+++ src/my_project/widget.py
@@ -25,7 +25,6 @@ def get_date(date_str: str) -> str:
 
 
 if __name__ == "__main__":
-    # Проверка работы функций
     print(mask_account_card("Visa Platinum 7000792289606361"))
     print(mask_account_card("Счет 73654108430135874305"))
     print(get_date("2024-03-11T02:26:18.671407"))
